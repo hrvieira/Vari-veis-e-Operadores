886ceedc58bccee3af8bd3a515ae3e8f846a330b
hrvieira <luizhrvieira@outlook.com>
2023-03-27 14:53:49 -0300

    toFixed

--- primeiroDesafio.js
+++ primeiroDesafio.js
@@ -1,12 +1,12 @@
 const precoCombustivel = 5.79;
 //const mediaGastoPorKM = (precoCombustivel / 10);
-const mediaGastoPorKM = 10;
-const distanciaDaViagemKM = 100;
+const mediaGastoPorKM = 12;
+const distanciaDaViagemKM = 1580;
 
 //const valorViagem = (mediaGastoPorKM * distanciaDaViagem)
 const valorViagem = (precoCombustivel / mediaGastoPorKM) * distanciaDaViagemKM;
 
-console.log(valorViagem);
+console.log(valorViagem.toFixed(2));
 
 
 // no vídeo em questão foi feito de outra maneira
